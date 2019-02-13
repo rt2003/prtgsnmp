f4e25c29c29fc7fd622775797fc01a601710e2ab
ubuntu <hualinkai@gmail.com>
2019-02-13 14:35:38 +0800

    20190213 v1

---
+++ prtgsnmp_install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Create by Robert Hua 2019/02/13
+if [ "$(id -u)" != "0" ]; then
+echo "This script must be run as root" 1>&2
+exit 1
+fi
+
+apt-get update
+sleep 3
+apt-get install -y apt
+sleep 3
+apt install -y snmpd
+sleep 3
+apt install -y snmp
+sleep 3
+apt install -y snmp-mibs-downloader
+sleep 3
+
